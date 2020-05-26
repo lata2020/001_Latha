bedae901011b4e13cd718c237dabfb65db26ba87
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2020-05-26 20:52:31 +0530

    Add files via upload

---
+++ 001_arith.py
@@ -0,0 +1,25 @@
+#Arithmetic Operators
+a = input("enter a int : ")
+b = input("ener b int : ")
+x = input("enter x float : ")
+y = input("ener y float : ")
+print a," + ", b, " ====>> ",a+b
+print x," + ", y, " ====>> ",x+y
+
+print a," - ", b, " ====>> ",a-b
+print x," - ", y, " ====>> ",x-y
+
+print a," * ", b, " ====>> ",a*b
+print x," * ", y, " ====>> ",x*y
+
+print a," % ", b, " ====>> ",a%b
+print x," % ", y, " ====>> ",x%y
+
+print a," / ", b, " ====>> ",a/b
+print x," / ", y, " ====>> ",x/y
+
+print a," // ", b, " ====>> ",a//b
+print x," // ", y, " ====>> ",x//y
+
+print a," ** ", b, " ====>> ",a**b
+print x," ** ", y, " ====>> ",x**y
